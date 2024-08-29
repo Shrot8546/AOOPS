7c11163ff99e6fb600796bf37dce522c84befb42
Shrot Kumar Pandey <152602259+Shrot8546@users.noreply.github.com>
2024-08-29 09:03:28 +0530

    Add files via upload

---
+++ 6suba/WEEK_6a/src/taskmanager/TAsk.java
@@ -0,0 +1,16 @@
+package taskmanager;
+public class TAsk {
+    private String description;
+
+    public TAsk(String description) {
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
